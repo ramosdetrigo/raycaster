6839fe4b2781507755861a2278b09fc4bceec696
Ramos <gu.ramos@proton.me>
2025-08-02 05:42:42 -0300

    raycaster

--- c_test/main.c
+++ c_test/main.c
@@ -1,8 +1,14 @@
 #include "raylib.h"
+#include <stdio.h>
 
 int main() {
+  SetTraceLogLevel(LOG_NONE);
   InitWindow(600, 400, "Hello, World!");
   SetTargetFPS(60);
+
+  
+  Image x = LoadImage("mia.png");
+  printf("width: %d, height: %d, format: %d\n", x.width, x.height, x.format);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use raylib::prelude::*;
+
+fn main() {
+    let (mut rl, thread) = raylib::init().size(1920, 1080).title("Hello, World").build();
+    rl.set_trace_log(TraceLogLevel::LOG_NONE);
+    // rl.set_target_fps(60);
+
+    let image = Image::gen_image_color(1920, 1080, Color::RED);
+    
+    while !rl.window_should_close() {
+        let texture = rl.load_texture_from_image(&thread, &image).unwrap();
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        d.draw_texture(&texture, 256, 0, Color::WHITE);
+        d.draw_text(format!("{}", d.get_fps()).as_str(), 12, 12, 20, Color::BLACK);
+        
+
+    }
+}
